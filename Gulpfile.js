e83039d63cf57a73b5e5a658184c5c3e183fc0de
bgaze <benjamin@bgaze.fr>
2019-03-15 20:31:07 +0400

    Update to Gulp 4 Improve populate function to manage errors

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,82 +1,106 @@
 // Dependencies.
-var fs = require('fs');
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var beautify = require('gulp-beautify');
-var uglify = require('gulp-uglify');
-var replace = require('gulp-replace');
-var prettyHtml = require('gulp-pretty-html');
-var through = require('through2');
-var pcl = require("pretty-console.log")
-
-// Enable prettified console.
-pcl.enable()
+const fs = require('fs');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const beautify = require('gulp-beautify');
+const uglify = require('gulp-uglify');
+const replace = require('gulp-replace');
+const prettyHtml = require('gulp-pretty-html');
+const through = require('through2');
+const chalk = require('chalk');
 
 // Declare populate engine.
 var populate = function (transforms) {
+    var content, replaced, errors = {};
+
+    var loadFile = function (path, escaped, type) {
+        let file = false;
+
+        if (!fs.existsSync(path)) {
+            errors[path] = chalk.redBright('           Missing file: ') + path;
+            return false;
+        }
+
+        file = fs.readFileSync(path, 'utf8');
+
+        if (transforms && typeof transforms[type] === 'function') {
+            return transforms[type](file, escaped, path);
+        }
+
+        if (transforms && typeof transforms === 'function') {
+            return transforms(file, escaped, path);
+        }
+
+        return file;
+    };
+
+    var populateWithFile = function (m) {
+        let pattern = m[0];
+        let path = m[2];
+        let type = m[3];
+        let escaped = (m[1] === '!');
+
+        let file = loadFile(path, escaped, type);
+        if (!file) {
+            return false;
+        }
+
+        let reg = new RegExp(pattern.replace(/\./g, '\\.').replace(/\//g, '\\/'), 'g');
+        content = content.replace(reg, file);
+        return true;
+    };
+
     return through.obj(function (input, encoding, callback) {
-        // Get stream content.
-        var content = String(input.contents);
+        content = String(input.contents);
+        replaced = true;
 
-        // Replace all template.
-        var m;
-        do {
-            m = /"(!)?(\.\/src\/.+\.(js|html))"/g.exec(content);
-            if (m) {
-                // Get template content.
-                let template = fs.readFileSync(m[2], 'utf8');
+        while (replaced) {
+            replaced = false;
+            let m, reg = /"(!)?(\.\/src\/.+\.(js|html))"/g;
 
-                // Transform template if transformer is provided.
-                if (transforms !== undefined) {
-                    if (typeof transforms[m[3]] === 'function') {
-                        template = transforms[m[3]](template, m[1] === '!');
-                    } else if (typeof transforms === 'function') {
-                        template = transforms(template, m[1] === '!', m[2]);
-                    }
+            while (m = reg.exec(content)) {
+                if (populateWithFile(m)) {
+                    replaced = true;
                 }
-
-                // Replace all occurences into content.
-                let reg = new RegExp(m[0].replace(/\./g, '\\.').replace(/\//g, '\\/'), 'g');
-                content = content.replace(reg, template);
             }
-        } while (m);
+        }
 
-        // Return new content as stream.
-        var output = input.clone();
+        for (let e in errors) {
+            console.log(errors[e]);
+        }
+
+        let output = input.clone();
         output.contents = new Buffer(content);
         callback(null, output);
     });
 };
 
 // Compile lib.
-gulp.task('lib', function () {
-    gulp.src('src/bootstrap4-dialogs.js')
+function lib() {
+    return  gulp.src('src/bootstrap4-dialogs.js')
             .pipe(populate({
                 html: function (content) {
                     return JSON.stringify(content.replace(/>\s+/g, '>').trim());
-                },
-                js: function (content) {
-                    return content.replace(/bsd\./g, 'this.');
-                }
+                } 
             }))
             .pipe(beautify.js({indent_size: 4}))
             .pipe(gulp.dest('dist'))
             .pipe(uglify())
             .pipe(rename({extname: '.min.js'}))
             .pipe(gulp.dest('dist'));
-});
+}
 
 // Compile demo.
-gulp.task('demo', function () {
-    gulp.src('src/demo.html')
+function demo() {
+    return gulp.src('src/demo.html')
             .pipe(populate(function (content, escaped, filename) {
                 if (escaped) {
                     return content.replace(/>/g, '&gt;').replace(/</g, '&lt;').trim();
                 }
 
-                var m = /^\.\/src\/templates\/(alert|confirm|prompt)\.html$/.exec(filename);
+                var m = /^\.\/src\/dialogs\/(alert|confirm|prompt)\.html$/.exec(filename);
                 if (m) {
-                    return '<a href="#' + m[1] + '">See below</a>';
+                    return '"[See below]"';
                 }
 
                 return content;
@@ -87,22 +111,21 @@ gulp.task('demo', function () {
                 extra_liners: []
             }))
             .pipe(gulp.dest('dist'));
-});
+}
 
 // Compile readme.
-gulp.task('readme', function () {
-    gulp.src('src/README.md')
+function readme() {
+    return gulp.src('src/README.md')
             .pipe(populate())
             .pipe(gulp.dest('.'));
-});
+}
 
 // Compile all files.
-gulp.task('dist', ['lib', 'demo', 'readme']);
 
-// Watch for file changes.
-gulp.task('watch', function (cb) {
-    gulp.watch('src/**/*', ['dist']);
+gulp.task('dist', gulp.series(lib, demo, readme));
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*', gulp.series('dist'));
 });
 
-// Default.
-gulp.task('default', ['dist', 'watch']);
+gulp.task('default', gulp.series('dist', 'watch'));
